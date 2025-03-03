eb8e1e29fb788fa761e218bc4927e2f550cdb4aa
Andrew Wong <andrew12345@live.com>
2025-03-03 14:39:11 +1100

    Initial commit

---
+++ src/main.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+const ChildProcess = std.process.Child;
+
+const LogStream = struct { eventMessage: []const u8, subsystem: []const u8, processID: c_int, timestamp: []const u8 };
+
+// TODO: Change these values to match the expected event structure
+const AppEvent = struct { timeString: []const u8, exampleBool: bool, exampleString: []const u8, exampleOptionalString: ?[]const u8 };
+
+const stdout = std.io.getStdOut().writer();
+const stderr = std.io.getStdErr().writer();
+
+pub fn emitEvent(event: AppEvent) !void {
+    // TODO: Change this to match the required log structure
+    try stdout.print("{s},{},{s}\n", .{ event.timeString, event.exampleBool, event.exampleString });
+}
+
+pub fn main() !void {
+    var allocatorBacking = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = allocatorBacking.allocator();
+
+    // TODO: Change this to the desired log predicate
+    const filter = "subsystem == 'com.apple.quicklook'";
+
+    var proc = ChildProcess.init(&[_][]const u8{
+        "/usr/bin/log", "stream", "--style", "ndjson",
+        // You may need to add --info or --debug to the log stream command
+        // "--info",
+        "--predicate",  filter,
+    }, allocator);
+    proc.stdout_behavior = ChildProcess.StdIo.Pipe;
+    proc.stderr_behavior = ChildProcess.StdIo.Ignore;
+    try proc.spawn();
+
+    // The max I've seen is around 5400 bytes
+    var buffer: [8192]u8 = undefined;
+
+    const reader = proc.stdout.?.reader();
+
+    // Skip the first line: "Filtering the log data using ..."
+    _ = try reader.readUntilDelimiter(&buffer, '\n');
+
+    try stderr.print("Observing events...\n", .{});
+
+    while (true) {
+        const bytesRead = (try reader.readUntilDelimiter(&buffer, '\n')).len;
+        const parsed = try std.json.parseFromSlice(LogStream, allocator, buffer[0..bytesRead], .{ .ignore_unknown_fields = true });
+        defer parsed.deinit();
+
+        // TODO: Fill in the event structure with your own application logic
+        var evtObject = AppEvent{ .timeString = parsed.value.timestamp, .exampleBool = true, .exampleString = "example string", .exampleOptionalString = null };
+
+        // e.g.
+        evtObject.exampleString = parsed.value.eventMessage;
+
+        try emitEvent(evtObject);
+    }
+}
